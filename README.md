4dc2e1aa01b4bc23be7b039dc85e7ec966b720b9
engafnanasem-pixel <eng.afnanasem@gmail.com>
2026-01-27 01:19:31 +0200

    Intial commit for calculaterv2

---
+++ README.md
@@ -0,0 +1,28 @@
+# The Fun Calculator 🧮🎉
+
+A simple Python calculator that performs basic mathematical operations using two numbers:
+
+- ➕ Addition  
+- ➖ Subtraction  
+- ✖ Multiply  
+- ➗ Division  
+
+✨ The program can save up to **five previous results** and display them when the user types `history`.
+
+---
+
+## How It Works ⚙️
+
+- The program asks the user to choose an operation.
+- If a mathematical operation is selected, the program asks for two numbers.
+- The user can type:
+  - `history` → to display the last five calculations  
+  - `exit` → to close the program
+
+---
+
+## How to Run ▶️
+
+```bash
+python3 calculator_v2.py
+
---
+++ calculaterv2.py
@@ -0,0 +1,94 @@
+history = []
+def add_history(text):
+    history.append(text)
+    if len(history) > 5 :
+       history.pop(0)
+def add (a,b):
+    return a+b ,"😄️"
+    
+def subtract (a,b):
+    return a-b , "🤓️"
+   
+def multiply (a,b) :
+   return a*b , "🤩️"
+   
+def divide (a,b) :
+   if b==0 :
+     return None , "🛑️/you can't divide by zero"
+   else :
+     return a/b , "😉️"
+
+def get_nums():
+   try: 
+      a = float(input("Enter the first number "))
+      b = float (input("Enter the second number"))
+      return a, b 
+   except ValueError:
+      print("❌️ Please Enter VALID number")
+      return None ,None 
+
+def main():
+   print ("Welcome to the FUN calculater")
+   while True :
+      print ("\n Choose an option :")
+      print ("🎗️ History")
+      print ("➕️ ADD")
+      print ("➖️ Subtract")
+      print ("✖️ Multiply")
+      print ("➗️ Divide")
+      print ("🧽️ Clear history")
+      print ("❌️ exit")
+      
+      
+      choice = input("Your Choice :").lower()
+      if choice not in ["+", "-" ,"*","/" ,"exit","history", "clear"] :
+         print ("⚠️ Invalid option try again")
+         continue
+      elif choice == "exit" :
+            print ("BYE BYE 👋️")
+            break
+      elif choice =="history" :
+            if not history :
+                print("😩️ NO HISTORY ")
+            else:
+                print("\nCalculation History:")
+                for i, h in enumerate(history, start=1):
+                   print(f"{i}. {h}")
+
+            continue
+      elif choice =="clear" :
+            if not history :
+                print("🤨️ NO HISTORY to be cleared")
+            else:
+                history.clear()
+                print ("\nThe history is CLEAR 💪️")
+
+            continue
+      else:
+         
+         num1 ,num2 =get_nums()
+         if num1 is None or num2 is None :
+            continue 
+         if choice == "+":
+            result , emoji = add(num1 ,num2 )
+         elif choice == "-":
+            result  , emoji = subtract(num1 ,num2 )
+         elif choice == "*":
+            result , emoji  = multiply(num1 ,num2 )
+         elif choice == "/":
+            result , emoji  = divide(num1 ,num2 )
+            if result is None :
+               print (emoji)
+               continue
+         
+         
+      
+      
+      operation_txt =f"{emoji} {num1} {choice} {num2} ={result}"
+      add_history(operation_txt)
+      #print (f"{emoji} The result = {result}")
+      print (operation_txt)
+
+
+main()
+      
